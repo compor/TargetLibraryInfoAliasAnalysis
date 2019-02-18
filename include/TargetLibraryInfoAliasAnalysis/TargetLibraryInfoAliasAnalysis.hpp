5050e8abf31ac84cc69906f894d08624bed6a375
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2019-02-18 10:11:34 +0000

    Add new pass manager support

--- include/TargetLibraryInfoAliasAnalysis/TargetLibraryInfoAliasAnalysis.hpp
+++ include/TargetLibraryInfoAliasAnalysis/TargetLibraryInfoAliasAnalysis.hpp
@@ -75,3 +75,23 @@ public:
 };
 
 } // namespace tliaa
+
+// TODO not sure if the new passmanager passes are required to be in the llvm
+// namespace if defined out of source tree
+namespace llvm {
+
+// Analysis pass providing a never-invalidated alias analysis result.
+class TLIAA : public AnalysisInfoMixin<TLIAA> {
+  friend AnalysisInfoMixin<TLIAA>;
+
+  static char PassID;
+
+public:
+  using Result = tliaa::TLIAAResult;
+
+  tliaa::TLIAAResult run(Function &F, AnalysisManager<Function> &AM) {
+    return tliaa::TLIAAResult(AM.getResult<TargetLibraryAnalysis>(F));
+  }
+};
+
+} // namespace llvm
